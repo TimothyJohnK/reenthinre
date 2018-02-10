57e2c4e1ee7be834dceb6be72cdfb036556af10d
TimothyJohnK <Tim@VectorLab.com>
2018-02-10 02:47:11 -0600

    updated App method handleInputChange

--- src/App.js
+++ src/App.js
@@ -116,7 +116,6 @@ export default class App extends Component {
   handleInputChange(event) {
     const target = event.target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
 
     this.setState({
       checked: value
